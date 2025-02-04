8b3b8dc6fafab65d0e9e609bfa6713d517d585f1
JabedAhmedRifat <jabedahmedrifat1@gmail.com>
2025-02-04 16:09:20 +0600

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import  { Route, BrowserRouter, Routes } from 'react-router-dom';
+import  { HashRouter as Router , Route, Routes } from 'react-router-dom';
 import Auth from './components/auth';
 import { CookiesProvider } from 'react-cookie';
 
@@ -13,12 +13,12 @@ function Root(){
 
         const routing = (
         <CookiesProvider>
-            <BrowserRouter>
+            <Router>
               <Routes>
                 <Route path="/" element={<Auth/>}/>
                 <Route path="/movies" element= {<App/>}/>
               </Routes>
-            </BrowserRouter>
+            </Router>
           </CookiesProvider>
         );
         return routing;
